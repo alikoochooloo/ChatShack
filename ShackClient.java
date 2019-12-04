616c4fe60a3ed3cbdd13315fbd776a6db2618e7a
Mohammadali <ali.saffary11@gmail.com>
2019-12-04 15:30:53 -0700

    more

--- ShackClient.java
+++ ShackClient.java
@@ -5,63 +5,87 @@
 
 import java.net.*;
 import java.io.*;
+import javax.swing.*;
 
 public class ShackClient
 {
-	public static final int DEFAULT_PORT = 1337;
+	// public static final int DEFAULT_PORT = 1337;
 	
-	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: java EchoClient <echo server>");
-			System.exit(0);
-		}
+	// public static void main(String[] args) throws IOException {
+	// 	if (args.length != 1) {
+	// 		System.err.println("Usage: java EchoClient <echo server>");
+	// 		System.exit(0);
+	// 	}
 		
-		BufferedReader networkBin = null;	// the reader from the network
-		PrintWriter networkPout = null;		// the writer to the network
-		BufferedReader localBin = null;		// the reader from the local keyboard
-		Socket sock = null;			// the socket
+	// 	BufferedReader networkBin = null;	// the reader from the network
+	// 	PrintWriter networkPout = null;		// the writer to the network
+	// 	BufferedReader localBin = null;		// the reader from the local keyboard
+	// 	Socket sock = null;			// the socket
 		
+	// 	try {
+	// 		sock = new Socket(args[0], DEFAULT_PORT);
+			
+	// 		// set up the necessary communication channels
+	// 		networkBin = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+	// 		localBin = new BufferedReader(new InputStreamReader(System.in));
+			
+	// 		/**
+	// 		 * a PrintWriter allows us to use println() with ordinary
+	// 		 * socket I/O. "true" indicates automatic flushing of the stream.
+	// 		 * The stream is flushed with an invocation of println()
+	// 		 */
+	// 		networkPout = new PrintWriter(sock.getOutputStream(),true);
+			
+	// 		/**
+	// 		 * Read from the keyboard and send it to the echo server.
+	// 		 * Quit reading when the client enters a period "."
+	// 		 */
+	// 		boolean done = false;
+	// 		while (!done) {
+	// 			String line = localBin.readLine();
+	// 			if (line.equals("."))
+	// 				done = true;
+	// 			else {
+	// 				networkPout.println(line);
+	// 				System.out.println("Server: " + networkBin.readLine());
+	// 			}
+	// 		}
+	// 	}
+	// 	catch (IOException ioe) {
+	// 		System.err.println(ioe);
+	// 	}
+	// 	finally {
+	// 		if (networkBin != null)
+	// 			networkBin.close();
+	// 		if (localBin != null)
+	// 			localBin.close();
+	// 		if (networkPout != null)
+	// 			networkPout.close();
+	// 		if (sock != null)
+	// 			sock.close();
+	// 	}
+	// }
+	Socket server;
+	BufferedReader fromServer;
+	ChatScreen screen;
+
+	public void ReaderThread(Socket server, ChatScreen screen) {
+		this.server = server;
+		this.screen = screen;
+	}
+	public void run() {
 		try {
-			sock = new Socket(args[0], DEFAULT_PORT);
-			
-			// set up the necessary communication channels
-			networkBin = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-			localBin = new BufferedReader(new InputStreamReader(System.in));
-			
-			/**
-			 * a PrintWriter allows us to use println() with ordinary
-			 * socket I/O. "true" indicates automatic flushing of the stream.
-			 * The stream is flushed with an invocation of println()
-			 */
-			networkPout = new PrintWriter(sock.getOutputStream(),true);
-			
-			/**
-			 * Read from the keyboard and send it to the echo server.
-			 * Quit reading when the client enters a period "."
-			 */
-			boolean done = false;
-			while (!done) {
-				String line = localBin.readLine();
-				if (line.equals("."))
-					done = true;
-				else {
-					networkPout.println(line);
-					System.out.println("Server: " + networkBin.readLine());
-				}
+			fromServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+
+			while (true) {
+				String message = fromServer.readLine();
+				String[] s = message.split("|");
+
+				// now display it on the display area
+				screen.displayMessage(message);
 			}
 		}
-		catch (IOException ioe) {
-			System.err.println(ioe);
-		}
-		finally {
-			if (networkBin != null)
-				networkBin.close();
-			if (localBin != null)
-				localBin.close();
-			if (networkPout != null)
-				networkPout.close();
-			if (sock != null)
-				sock.close();
-		}
+		catch (IOException ioe) { System.out.println(ioe); }
+
 	}
 }
--- bigG.java
+++ bigG.java
@@ -11,6 +11,7 @@ import java.awt.*;
 import java.awt.event.*; //user clicking buttons
 import java.util.Calendar;
 import java.util.Date;
+import java.io.*;
 import java.net.*;
 
 
@@ -21,28 +22,47 @@ public class bigG extends JFrame{
     private JTextArea comment, content;
     private JButton respect, connectivity;
     private ListenForButton lfb; 
-    private JPanel p;
-    private JPanel poo;
+    private JPanel p, poo, pee;
+    // private JPanel ;
     private boolean concheck = true;
-    public Socket client;
+    private JScrollPane scroll1, scroll2;
+    private JComboBox guys;
+    // public Socket client;
+    public Socket annoy;
+    public DataOutputStream toServer;
+
     // public OutputStream toClient = null;
     public static void main(String[] args){
         // this.client = client
         new bigG();
+        /*
+        try {
+            annoy = new Socket(args[0], 1337);
+            toServer = new DataOutputStream(annoy.getOutputStream());
+			bigG win = new bigG();
+			win.displayMessage("My name is " + args[1]);
+
+			Thread ShackClient = new Thread(new ShackClient(annoy, win));
+
+			ShackClient.run();
+		}
+		catch (UnknownHostException uhe) { System.out.println(uhe); }
+        catch (IOException ioe) { System.out.println(ioe); }
+        */
     }
 
-    public void runGui(Socket client){
-        this.client = client;
-        // toClient = new BufferedOutputStream(client.getOutputStream());
-        new bigG();
+    // public void runGui(Socket client){
+    //     this.client = client;
+    //     // toClient = new BufferedOutputStream(client.getOutputStream());
+    //     new bigG();
 
-    } 
+    // } 
 
 
     public bigG(){
         // Toolkit tk = Toolkit.getDefaultToolkit();      
         // Dimension dim = tk.getScreenSize(); 
-        this.setSize(1000, 800);
+        this.setSize(1000, 825);
         this.setLocationRelativeTo(null);        
         this.setTitle("Whacha Thinking");
         font = new Font("Times New Roman", Font.PLAIN, 20);
@@ -50,14 +70,21 @@ public class bigG extends JFrame{
         // JPanel p = new JPanel(new GridLayout(0,2)); 
         p = new JPanel();
         poo = new JPanel();
+        pee = new JPanel();
         p.setBackground(Color.lightGray);
+        poo.setBackground(Color.BLUE);
+        pee.setBackground(Color.ORANGE);
 
-        comment = new JTextArea(4,50);
-        // comment.setSize(200, 200);
-        // comment.resize(200, 200);
+
+        comment = new JTextArea(4,60);
         comment.setToolTipText("type your comment");
         comment.setFont(font); 
+        comment.requestFocus();
         p.add(comment);
+
+        scroll2 = new JScrollPane(comment);
+        scroll2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        p.add(scroll2);
 
         content = new JTextArea(25,60);
         content.setEditable(false);
@@ -65,23 +92,32 @@ public class bigG extends JFrame{
         content.setFont(font); 
         poo.add(content);
 
+        scroll1 = new JScrollPane(content);
+        scroll1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        poo.add(scroll1);
+        
         lfb = new ListenForButton();
 
-        this.respect = new JButton("press F to respect");
+
+        guys = new JComboBox();
+        guys.addItem("all");
+        pee.add(guys);
+
+        this.respect = new JButton("press F for respect");
         respect.setToolTipText("yeet you dickwad");
         respect.addActionListener(lfb);
         respect.setFont(font);
-        p.add(respect);
+        pee.add(respect);
 
         this.connectivity = new JButton();
         connectivity.setText("Join");
         connectivity.addActionListener(lfb);
         connectivity.setToolTipText("join you dickwad");
         connectivity.setFont(font);
-        p.add(connectivity);
+        pee.add(connectivity);
 
-        this.add(p,BorderLayout.SOUTH); 
-
+        this.add(p,BorderLayout.CENTER); 
+        this.add(pee,BorderLayout.SOUTH);
         this.add(poo,BorderLayout.NORTH);
         // this.pack();
         this.setVisible(true);
@@ -90,27 +126,35 @@ public class bigG extends JFrame{
     public class ListenForButton implements ActionListener{
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == respect) {
-                System.out.println(comment.getText());
+                // System.out.println(comment.getText());
                 if(!comment.getText().equals("")){
                     content.append("you: " + comment.getText() + "\n");
                     comment.setText("");
-                    // functionality to send text to client
-                    // client.
+                    comment.requestFocus();
+                    // functionality to send text to server
+                    // result = "BDMG|username|all|date\r\ncontent\r\n"
+                    // toServer.write(result.getBytes());
                     
                 }
             }
             else if (e.getSource() == connectivity) {
                 if (concheck){
                     connectivity.setText("Leave");
-                    System.out.println("yes");
+                    // System.out.println("yes");
                     concheck = false;
                     // functionality to join
+                    // username = comment.getText()
+                    // date = 
+                    // result = "JOIN|username|all|date"
+                    // toServer.write(result.getBytes());
                 }
                 else {                    
-                    System.out.println("no");
+                    // System.out.println("no");
                     concheck = true;
                     connectivity.setText("Join");
                     // functionality to leave
+                    // result = "LEAV|username|all|date"
+                    // toServer.write(result.getBytes());
                 }
             }
         }
